77be0c5ee908e832ec25f14047fd365b63b72a6e
Luis Ruiz <31607146+egm42@users.noreply.github.com>
2023-06-21 20:19:10 -0400

    update PORT env variable name

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const crypto = require("crypto");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-const PORT = process.env.SERVER_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const env = process.env.NODE_ENV || 'development'
 
 aws.config.update({
