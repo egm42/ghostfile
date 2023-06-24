998f4d8294ae1a6adcaaf42cb315d10a62a72691
egm42 <31607146+egm42@users.noreply.github.com>
2023-06-24 11:40:29 -0400

    Added file size to download details page and refactored how file details are passed down in components

--- client/src/pages/Download.js
+++ client/src/pages/Download.js
@@ -15,11 +15,11 @@ class Download extends React.Component {
   renderForm() {
     switch(this.props.downloadStatus) {
       case DownloadStatus.FILEFOUND:
-        return <DownloadForm downloadStatus={this.props.downloadStatus} downloadFileName={this.props.downloadFileName} downloadFile={this.props.downloadFile} ttl={this.props.ttl}/>
+        return <DownloadForm fileDetails={this.props.fileDetails} downloadFile={this.props.downloadFile} />
       case DownloadStatus.DOWNLOADSUCCESS:
         return <DownloadSuccess />
       case DownloadStatus.DOWNLOADING:
-        return <Downloading downloadFileName={this.props.downloadFileName} downloadProgress={this.props.downloadProgress}/>
+        return <Downloading downloadFileName={this.props.fileDetails.originalname} downloadProgress={this.props.downloadProgress}/>
       case DownloadStatus.FILENOTFOUND:
       case DownloadStatus.DOWNLOADFAILED:
       default:
