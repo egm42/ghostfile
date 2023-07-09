10e97125d2500e342b7426fcd38efec154f89527
Luis Ruiz <31607146+egm42@users.noreply.github.com>
2023-07-09 14:40:41 -0400

    fixed test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,7 @@
-import { screen, toBeInTheDocument } from '@testing-library/jest-dom'
-import { render, getByText } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('upload page', () => {
-  const container = render(<App />);
-  const linkElement = getByText(container, 'Select and upload a file to create a one-time download link');
-  expect(linkElement).toBeInTheDocument();
+  render(<App />);
+  expect(screen.getByText('Select and upload a file to create a one-time download link. File uploads are limited to 2GB.')).toBeInTheDocument();
 });
